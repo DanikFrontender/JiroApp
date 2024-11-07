bf1beffdae0856f840e291f25f49d175a54405ed
Danik <dfrontender@gmail.com>
2024-11-07 18:39:24 +0400

    2:fixed bugs

--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -1,5 +1,5 @@
 import { toast } from "sonner";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { InferRequestType, InferResponseType } from "hono";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
@@ -9,7 +9,7 @@ type ResponseType = InferResponseType<(typeof client.api.workspaces)["$post"]>;
 type RequestType = InferRequestType<(typeof client.api.workspaces)["$post"]>;
 
 export const useCreateWorkspace = () => {
-  const router = useRouter();
+  // const router = useRouter();
   const queryClient = useQueryClient();
 
   const mutation = useMutation<ResponseType, Error, RequestType>({
@@ -25,7 +25,7 @@ export const useCreateWorkspace = () => {
     onSuccess: () => {
       toast.success("Workspace created");
 
-      router.refresh();
+      // router.refresh();
       queryClient.invalidateQueries({ queryKey: ["workspaces"] });
     },
     onError: () => {
